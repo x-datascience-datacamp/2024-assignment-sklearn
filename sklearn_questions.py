19d016ea290ed7815e6d17776d72b5e33cacaab7
Sheeepycheap <alinejadkevin@gmail.com>
2024-12-17 15:31:42 +0100

    UP my solution

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -163,6 +163,20 @@ class MonthlySplit(BaseCrossValidator):
     def __init__(self, time_col='index'):  # noqa: D107
         self.time_col = time_col
 
+    def _prepare_data(self, X):
+        """Prepare and validate the data, helper function."""
+        X_copy = X.copy()
+        if self.time_col == 'index':
+            X_copy = X_copy.reset_index()
+        # Validate datetime type
+        if not pd.api.types.is_datetime64_any_dtype(X_copy[self.time_col]):
+            raise ValueError(
+                f"The column '{self.time_col}' is not a datetime."
+                )
+        # Sort by time column
+        X_copy = X_copy.sort_values(by=self.time_col)
+        return X_copy
+
     def get_n_splits(self, X, y=None, groups=None):
         """Return the number of splitting iterations in the cross-validator.
 
@@ -181,16 +195,9 @@ class MonthlySplit(BaseCrossValidator):
         n_splits : int
             The number of splits.
         """
-        X_copy = X.copy()
-        if self.time_col == 'index':
-            X_copy = X_copy.reset_index()
-        # Validate datetime type
-        if not pd.api.types.is_datetime64_any_dtype(X_copy[self.time_col]):
-            raise ValueError(
-                f"The column '{self.time_col}' is not a datetime."
-                )
-        # Sort by time column
-        X_copy = X_copy.sort_values(by=self.time_col)
+        X_copy = self._prepare_data(X)
+        # Identify monthly boundaries using .dt.to_period('M')
+        # for better control
         unique_months = X_copy[self.time_col].dt.to_period('M').unique()
         return len(unique_months) - 1  # Subtract 1 for training-test split
 
